c7cef8c236c84ee1d91e4e33afce30952b4b5702
BCannard <cannard.bastien@gmail.com>
2023-12-27 16:52:00 +0100

    Add some files

--- main.cpp
+++ main.cpp
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-#include "BSmodel.h"
-#include <iostream>
-#include <vector>
-
-int main() {
-    // Exemple de paramètres pour l'initialisation
-    double time = 0.0;
-    double stockPrice = 100.0;
-    double volatility = 0.2;
-    double drift = 0.05;
-    double riskFreeRate = 0.03;
-
-    // Créer une instance de BlackScholes
-    BlackScholes bs(time, stockPrice, volatility, drift, riskFreeRate);
-
-    // Générer un chemin de prix
-    double targetDate = 1.0; // Par exemple, 1 an plus tard
-    int steps = 10; // Nombre de pas dans le chemin de prix
-    std::vector<double> pricePath = bs.pricePath(targetDate, steps);
-
-    // Afficher le chemin de prix
-    std::cout << "Price Path:" << std::endl;
-    for (double price : pricePath) {
-        std::cout << price << std::endl;
-    }
-=======
 #include <iostream>
 #include "Option.h"
 #include "BSmodel.h"
@@ -33,7 +6,6 @@ int main() {
 int main(){
 
     EuroCall call(2.0,40.0);
->>>>>>> 7c00c40ec3fcb94e740bf5ce22caa01dddbaba96
 
     return 0;
 }
