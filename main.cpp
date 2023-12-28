c02e5a52e67c099637b3b26a16524e884fdfeec1
jallier7 <jerome.allier@ensae.fr>
2023-12-28 20:28:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include "Option.h"
-#include "BSmodel.h"
+#include "BinomialModel.h"
 
 int main() {
-    EuroCall call(2.0, 40.0);
+    // Parameters for the options and the model
+    double spotPrice = 100; // Example spot price of the underlying asset
+    double strikePrice = 100; // Example strike price
+    double maturity = 1; // Time to maturity in years
+    double interestRate = 0.05; // Risk-free interest rate
+    double volatility = 0.2; // Volatility of the underlying asset
+    int numSteps = 100; // Number of steps in the binomial model
 
-    std::cout << "Program executed successfully." << std::endl;
+    // Create the European Call and Put options
+    EuroCall euroCall(maturity, strikePrice);
+    EuroPut euroPut(maturity, strikePrice);
+
+    // Create the American Call and Put options
+    AmeriCall ameriCall(maturity, strikePrice);
+    AmeriPut ameriPut(maturity, strikePrice);
+
+    // Create BinomialModel instances for European and American options
+    BinomialModel binomialModelEuro(spotPrice, interestRate, volatility, maturity, numSteps, false); // false for European
+    BinomialModel binomialModelAmeri(spotPrice, interestRate, volatility, maturity, numSteps, true); // true for American
+
+    // Pricing European options
+    double euroCallPrice = binomialModelEuro.priceOption(euroCall);
+    double euroPutPrice = binomialModelEuro.priceOption(euroPut);
+
+    // Pricing American options
+    double ameriCallPrice = binomialModelAmeri.priceOption(ameriCall);
+    double ameriPutPrice = binomialModelAmeri.priceOption(ameriPut);
+
+    // Output the results
+    std::cout << "European Call Option Price: " << euroCallPrice << std::endl;
+    std::cout << "European Put Option Price: " << euroPutPrice << std::endl;
+    std::cout << "American Call Option Price: " << ameriCallPrice << std::endl;
+    std::cout << "American Put Option Price: " << ameriPutPrice << std::endl;
 
     return 0;
 }
