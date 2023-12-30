0e0a9b203bd15ec2dcbe9b0d6be685720ee97371
Théo Moret <85231997+theo-moret@users.noreply.github.com>
2023-12-30 10:31:15 +0100

    Headers .h

---
+++ include/BSmodel.h
@@ -0,0 +1,44 @@
+#ifndef BSMODEL_H
+#define BSMODEL_H
+
+#include <iostream>
+#include <vector>
+
+class BlackScholes{
+
+    private:
+
+    double time;
+    double stockPrice;
+    double volatility;
+    double drift;
+    double riskFreeRate;
+
+    public:
+
+    BlackScholes();
+    BlackScholes(double time, double stockPrice, double volatility,
+    double drift, double riskFreeRate);
+    ~BlackScholes(){};
+
+    void set_time(double time);
+    double get_time() const;
+
+    void set_stockPrice(double stockPrice);
+    double get_stockPrice() const;
+
+    void set_volatility(double volatility);
+    double get_volatility() const;
+
+    void set_drift(double drift);
+    double get_drift() const;
+
+    void set_riskFreeRate(double riskFreeRate);
+    double get_riskFreeRate() const;
+
+    std::vector<double> pricePath(double date, int steps) const;
+    std::vector<double> pricePathNeutral(double date, int steps) const;
+
+};
+
+#endif
